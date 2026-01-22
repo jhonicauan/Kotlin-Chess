1d9b8e667a1835780301bc83b3c87f92acfb276d
Jhoni Cauan <jhonielias8@gmail.com>
2026-01-22 14:50:33 -0300

    Metódo de clone adicionado

--- backend/src/main/kotlin/com/jhonibruno/ChessKotlinWebsocket/models/Move.kt
+++ backend/src/main/kotlin/com/jhonibruno/ChessKotlinWebsocket/models/Move.kt
@@ -2,4 +2,8 @@ package com.jhonibruno.ChessKotlinWebsocket.models
 
 import com.jhonibruno.ChessKotlinWebsocket.models.board.Slot
 
-data class Move(val initialSlot: Slot,val destinationSlot: Slot,val isCapture: Boolean)
+data class Move(val pieceSlot: Slot,val destinationSlot: Slot,val isCapture: Boolean) {
+    fun clone(): Move {
+        return Move(pieceSlot.clone(), destinationSlot.clone(), isCapture)
+    }
+}
