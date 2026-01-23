27fa68a59738ed206539146569c7e2c1f5aeabe9
Jhoni Cauan <jhonielias8@gmail.com>
2026-01-23 13:48:25 -0300

    Foi adicionado moveLog para manter registro da partida, isCheck foi modificado de função para variavel e todas suas menções foram alteradas para a var isCheck, Correção de bug da vitoria de cores opostas, Função para gerar o DTO de jogo criada, Função para ver movimentos possiveis criada.

--- backend/src/main/kotlin/com/jhonibruno/ChessKotlinWebsocket/models/Game.kt
+++ backend/src/main/kotlin/com/jhonibruno/ChessKotlinWebsocket/models/Game.kt
@@ -1,51 +1,77 @@
 package com.jhonibruno.ChessKotlinWebsocket.models
 
 import com.jhonibruno.ChessKotlinWebsocket.models.board.Board
+import com.jhonibruno.ChessKotlinWebsocket.models.client.ClientBoardDTO
+import com.jhonibruno.ChessKotlinWebsocket.models.client.GameDTO
+import com.jhonibruno.ChessKotlinWebsocket.models.client.GameStatusDTO
 import com.jhonibruno.ChessKotlinWebsocket.models.enums.GameStatus
 import com.jhonibruno.ChessKotlinWebsocket.models.enums.PieceColor
+import com.jhonibruno.ChessKotlinWebsocket.models.history.MoveLogRegistry
 import com.jhonibruno.ChessKotlinWebsocket.models.notation.MoveNotation
 
 class Game(private val board: Board) {
 
-    var currentPlayerColor: PieceColor = PieceColor.WHITE
-    var status = GameStatus.RUNNING
+    private var currentPlayerColor: PieceColor = PieceColor.WHITE
+    private var status = GameStatus.RUNNING
+    private val moveLog = mutableListOf<MoveLogRegistry>()
+    private var isCheck = false
 
     private fun changePlayerColor() {
         currentPlayerColor = if (currentPlayerColor == PieceColor.WHITE) PieceColor.BLACK else PieceColor.WHITE
     }
 
-    private fun isCheck(): Boolean {
-        return board.isKingInCheck(currentPlayerColor) && board.existsSafeMoves(currentPlayerColor)
+    private fun verifyCheck() {
+        isCheck = board.isKingInCheck(currentPlayerColor)
     }
 
     private fun isCheckmate(): Boolean {
-        return board.isKingInCheck(currentPlayerColor) && !board.existsSafeMoves(currentPlayerColor)
+        return isCheck && !board.existsSafeMoves(currentPlayerColor)
     }
 
     private fun isStalemate(): Boolean {
-        return !board.isKingInCheck(currentPlayerColor) && !board.existsSafeMoves(currentPlayerColor)
+        return !isCheck && !board.existsSafeMoves(currentPlayerColor)
     }
 
     fun updateStatus() {
         if (isCheckmate()) {
             status = when(currentPlayerColor) {
-                PieceColor.WHITE -> GameStatus.WHITE_WINS
-                PieceColor.BLACK -> GameStatus.BLACK_WINS
+                PieceColor.WHITE -> GameStatus.BLACK_WINS
+                PieceColor.BLACK -> GameStatus.WHITE_WINS
             }
         }
         else if (isStalemate()) status = GameStatus.STALEMATE
-
-        if (status == GameStatus.BLACK_WINS) println("Vitoria das pretas")
-        if (status == GameStatus.WHITE_WINS) println("Vitoria das brancas")
-        if (status == GameStatus.STALEMATE) println("Empate")
     }
 
     private fun existsPossibleMove(move: Move, possibleMoves: List<Move>): Boolean {
         return possibleMoves.contains(move)
     }
 
-    fun isRunning(): Boolean {
+    private fun getBoard(): List<ClientBoardDTO> {
+        return board.getSlots().flatten().map { it.toDto() }
+    }
+
+    fun getGameDTO(): GameDTO {
+        return GameDTO(getBoard(),moveLog.map { it.toDto() }, GameStatusDTO(isCheck, status, currentPlayerColor))
+    }
+
+    fun viewPossibleMoves(position: String): List<ClientBoardDTO> {
+        val slot = board.getSlotByPosition(position)
+        val legalMoves = board.getLegalMoves(slot)
+        val legalSlots = mutableListOf<ClientBoardDTO>()
+        for (move in legalMoves) {
+            legalSlots.add(move.destinationSlot.toDto())
+        }
+        return legalSlots
+    }
+
+    private fun isRunning(): Boolean {
         return status == GameStatus.RUNNING
+    }
+
+    private fun getRound(): Int {
+        if(moveLog.isEmpty()) return 1
+        return if (moveLog.size % 2  != 0) moveLog.last().round
+        else moveLog.last().round + 1
     }
 
     fun makeMove(piecePosition: String, destinationPosition: String) {
@@ -59,19 +85,15 @@ class Game(private val board: Board) {
         val isCapture = destinationSlot.piece != null
         val move = Move(pieceSlot, destinationSlot, isCapture)
         val possibleMoves = board.getLegalMoves(pieceSlot)
+
         if (existsPossibleMove(move, possibleMoves)) {
             val moveClone = move.clone()
             val alternativePiece = board.getAlternativePiece(move)
             board.movePiece(move)
-
             changePlayerColor()
-
-            if (isCheck())  println("O rei esta em xeque")
-
-            val moveNotation = MoveNotation(moveClone, alternativePiece, isCheck(), isCheckmate(), false, false, null)
-            println(moveNotation.notation)
-
-            board.showBoard()
+            verifyCheck()
+            val moveNotation = MoveNotation(moveClone, alternativePiece, isCheck, isCheckmate(), false, false, null)
+            moveLog.add(MoveLogRegistry(getRound(),moveNotation))
             updateStatus()
         }
     }
