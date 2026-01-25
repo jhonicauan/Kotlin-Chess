49fea18e32c13cc6a9a17b0ef7a5870d4b1fdf34
Jhoni Cauan <jhonielias8@gmail.com>
2026-01-25 05:33:07 -0300

    Promoção e roque adicionados ao tabuleiro

--- backend/src/main/kotlin/com/jhonibruno/ChessKotlinWebsocket/models/board/Board.kt
+++ backend/src/main/kotlin/com/jhonibruno/ChessKotlinWebsocket/models/board/Board.kt
@@ -12,7 +12,6 @@ import com.jhonibruno.ChessKotlinWebsocket.models.validators.PawnValidator
 class Board {
     private val slots: MutableList<MutableList<Slot>> = mutableListOf()
     private val columns: List<Char> = listOf('a','b','c','d','e','f','g','h')
-
     private fun generateBoard() {
         if (!slots.isEmpty()) return
 
@@ -74,15 +73,30 @@ class Board {
         val pieceSlot = move.pieceSlot
         val destinationSlot = move.destinationSlot
         slots[destinationSlot.row][destinationSlot.column].piece = slots[pieceSlot.row][pieceSlot.column].piece
-        val piece = slots[destinationSlot.row][destinationSlot.column].piece
+        val piece = slots[destinationSlot.row][destinationSlot.column].piece ?: return
+
         if(piece?.pieceType == PieceType.PAWN) (piece as Pawn).isMoved = true
-        if(piece?.pieceType == PieceType.KING) (piece as King).canCastling = false
-        if(piece?.pieceType == PieceType.ROOK) (piece as Rook).canCastling = false
+        if(piece?.pieceType == PieceType.KING) (piece as King).isMoved = true
+        if(piece?.pieceType == PieceType.ROOK) (piece as Rook).isMoved = true
         slots[pieceSlot.row][pieceSlot.column].piece = null
     }
 
+     fun canPromote(destination: Slot,piece: Piece): Boolean {
+        val promotionRow = if(piece.color == PieceColor.WHITE)  7 else 0
+        return piece.pieceType == PieceType.PAWN && promotionRow == destination.row
+    }
 
-
+    fun promotePawn(destination: Slot, type: PieceType){
+        if(destination.piece == null) throw IllegalArgumentException("Não se pode promover casas nulas")
+        val color = destination.piece?.color ?: throw IllegalArgumentException("Cor invalida")
+        when(type){
+            PieceType.KNIGHT -> destination.piece = Knight(color)
+            PieceType.BISHOP -> destination.piece = Bishop(color)
+            PieceType.ROOK -> destination.piece = Rook(color)
+            PieceType.QUEEN -> destination.piece = Queen(color)
+            else -> throw IllegalArgumentException("PROMOÇÃO INVALIDA")
+        }
+    }
 
     private fun reverseMove(move: Move) {
         val pieceSlot = move.pieceSlot
@@ -131,8 +145,7 @@ class Board {
         val enemyColor = if (color == PieceColor.WHITE) PieceColor.BLACK else PieceColor.WHITE
         val enemiesSlots = getSlotsByColor(enemyColor)
         for (enemySlot in enemiesSlots) {
-            val captureMove = Move(enemySlot, slot, true)
-            if (getPossibleMoves(enemySlot).contains(captureMove)) return false
+            if (getPossibleMoves(enemySlot).any{it.destinationSlot == slot}) return false
         }
         return true
     }
@@ -161,6 +174,80 @@ class Board {
                     && it.piece?.pieceType == pieceSlot.piece?.pieceType
                     && (it.row == pieceSlot.row || it.column == pieceSlot.column)
                     && getLegalMoves(it).contains(Move(it,destinationSlot,isCapture))}
+    }
+
+    private fun kingSideIsSafe(color: PieceColor): Boolean {
+        if (color == PieceColor.WHITE) return verifySlotIsSafe(color,slots[0][5]) && verifySlotIsSafe(color,slots[0][6])
+        return verifySlotIsSafe(color,slots[7][5]) && verifySlotIsSafe(color,slots[7][6])
+    }
+
+    private fun kingSideIsFree(color: PieceColor): Boolean {
+        if (color == PieceColor.WHITE) return slots[0][5].piece == null && slots[0][6].piece == null
+        return slots[7][5].piece == null && slots[7][6].piece == null
+    }
+
+    private fun kingSideStillInThePlace(color: PieceColor): Boolean {
+        if (color == PieceColor.WHITE) {
+            if (slots[0][4].piece == null || slots[0][7].piece == null) return false
+            if (slots[0][4].piece?.pieceType != PieceType.KING || slots[0][7].piece?.pieceType != PieceType.ROOK) return false
+            val king = slots[0][4].piece as King
+            val rook = slots[0][7].piece as Rook
+            return !king.isMoved && !rook.isMoved
+        }
+        if (slots[7][4].piece == null || slots[7][7].piece == null) return false
+        if (slots[7][4].piece?.pieceType != PieceType.KING || slots[7][7].piece?.pieceType != PieceType.ROOK) return false
+        val king = slots[7][4].piece as King
+        val rook = slots[7][7].piece as Rook
+        return !king.isMoved && !rook.isMoved
+    }
+
+    fun canKingSideCastling(color: PieceColor): Boolean {
+        return kingSideStillInThePlace(color) && kingSideIsFree(color) && kingSideIsSafe(color) && !isKingInCheck(color)
+    }
+
+    fun kingSideCastling(color: PieceColor) {
+        val colorRow = if (color == PieceColor.WHITE) 0 else 7
+        slots[colorRow][4].piece = null
+        slots[colorRow][7].piece = null
+        slots[colorRow][6].piece = King(color,true)
+        slots[colorRow][5].piece = Rook(color,true)
+    }
+
+    private fun queenSideIsSafe(color: PieceColor): Boolean {
+        if (color == PieceColor.WHITE) return verifySlotIsSafe(color,slots[0][3]) && verifySlotIsSafe(color,slots[0][2])
+        return verifySlotIsSafe(color,slots[7][3]) && verifySlotIsSafe(color,slots[7][2])
+    }
+
+    private fun queenSideIsFree(color: PieceColor): Boolean {
+        if (color == PieceColor.WHITE) return slots[0][3].piece == null && slots[0][2].piece == null && slots[0][1].piece == null
+        return slots[7][3].piece == null && slots[7][2].piece == null && slots[7][1].piece == null
+    }
+
+    private fun queenSideStillInThePlace(color: PieceColor): Boolean {
+        if (color == PieceColor.WHITE) {
+            if (slots[0][4].piece == null || slots[0][0].piece == null) return false
+            if (slots[0][4].piece?.pieceType != PieceType.KING || slots[0][0].piece?.pieceType != PieceType.ROOK) return false
+            val king = slots[0][4].piece as King
+            val rook = slots[0][0].piece as Rook
+            return !king.isMoved && !rook.isMoved
+        }
+        if (slots[7][4].piece == null || slots[7][0].piece == null) return false
+        if (slots[7][4].piece?.pieceType != PieceType.KING && slots[7][0].piece?.pieceType != PieceType.ROOK) return false
+        val king = slots[7][4].piece as King
+        val rook = slots[7][0].piece as Rook
+        return !king.isMoved && !rook.isMoved
+    }
+
+    fun canQueenSideCastling(color: PieceColor): Boolean {
+        return queenSideStillInThePlace(color) && queenSideIsFree(color) && queenSideIsSafe(color) && !isKingInCheck(color)
+    }
+
+    fun queenSideCastling(color: PieceColor) {
+        val colorRow = if (color == PieceColor.WHITE) 0 else 7
+        slots[colorRow][4].piece = null
+        slots[colorRow][0].piece = null
+        slots[colorRow][2].piece = King(color,true)
+        slots[colorRow][3].piece = Rook(color,true)
     }
 
     init {
