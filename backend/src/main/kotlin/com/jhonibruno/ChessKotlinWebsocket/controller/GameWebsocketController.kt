106d3825a50ae03b077bf7241d27b55a9c565899
Jhoni Cauan <jhonielias8@gmail.com>
2026-01-25 05:35:58 -0300

    Jogo por salas criado

--- backend/src/main/kotlin/com/jhonibruno/ChessKotlinWebsocket/controller/GameWebsocketController.kt
+++ backend/src/main/kotlin/com/jhonibruno/ChessKotlinWebsocket/controller/GameWebsocketController.kt
@@ -8,6 +8,7 @@ import com.jhonibruno.ChessKotlinWebsocket.models.board.Slot
 import com.jhonibruno.ChessKotlinWebsocket.models.client.ClientBoardDTO
 import com.jhonibruno.ChessKotlinWebsocket.models.client.GameDTO
 import com.jhonibruno.ChessKotlinWebsocket.models.client.GameStatusDTO
+import org.springframework.messaging.handler.annotation.DestinationVariable
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.messaging.simp.annotation.SendToUser
@@ -17,24 +18,34 @@ import org.springframework.stereotype.Controller
 @MessageMapping("/game")
 class GameWebsocketController {
 
-    val game = Game(Board())
+    val games: MutableMap<String, Game>  = mutableMapOf()
 
-    @MessageMapping("/joined")
-    @SendTo("/topic/game")
-    fun joined(): GameDTO {
+    @MessageMapping("/create")
+    fun createGame(gameName: String) {
+        if (games.containsKey(gameName)) return
+        println("Vamos criar")
+        games[gameName] = Game(Board())
+    }
+
+    @MessageMapping("/joined/{gameName}")
+    @SendTo("/topic/game/{gameName}")
+    fun joined(@DestinationVariable gameName: String): GameDTO {
+        val game = games[gameName] ?: throw IllegalArgumentException("Game $gameName not found")
         return game.getGameDTO()
     }
 
-    @MessageMapping("/checkMoves")
-    @SendToUser("/queue/check")
-    fun checkMoves(move: String): List<ClientBoardDTO> {
+    @MessageMapping("/checkMoves/{gameName}")
+    @SendToUser("/queue/check/{gameName}")
+    fun checkMoves(@DestinationVariable gameName: String, move: String): List<ClientBoardDTO> {
+        val game = games[gameName] ?: throw IllegalArgumentException("Game $gameName not found")
         return game.viewPossibleMoves(move)
     }
 
-    @MessageMapping("/makeMove")
-    @SendTo("/topic/game")
-    fun makeMove(move: ClientMoveDTO): GameDTO {
-        game.makeMove(move.position, move.destiny)
+    @MessageMapping("/makeMove/{gameName}")
+    @SendTo("/topic/game/{gameName}")
+    fun makeMove(@DestinationVariable gameName: String, move: ClientMoveDTO): GameDTO {
+        val game = games[gameName] ?: throw IllegalArgumentException("Game $gameName not found")
+        game.makeMove(move)
         return game.getGameDTO()
     }
 
